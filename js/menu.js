ec0e6613ffa8e49558e4fc4e5152d8fff5de76f3
Renato Priolo <rpriolo@gmail.com>
2022-09-09 17:11:21 -0300

    Adição de botão dinâmico na navbar

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+let botaoMenu = document.querySelector('.navbar-toggler');
+
+botaoMenu.addEventListener('click', () => {
+    let iconeMenu = document.querySelector('i.fa-solid.fa-bars.icone-menu')
+    iconeMenu.classList.toggle('fa-xmark');
+})
